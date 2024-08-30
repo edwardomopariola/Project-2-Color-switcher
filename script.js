46608a031d01454570014adbc392d334f85b5f46
Edward Omopariola <femiedjr@gmail.com>
2024-08-30 07:58:39 -0400

    few changed on html and js file

---
+++ script.js
@@ -0,0 +1,7 @@
+
+  function selectedColor(value) {
+    document.body.style.backgroundColor = value;
+    document.querySelectorAll("h5")[0].style.color="yellow"; //I access the element with query seleector all
+  }
+ 
+
